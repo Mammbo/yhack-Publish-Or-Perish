aeee6aa2f70b56113f420c8203951cefabfa74c8
Tue Anh Tran <tueanh.tran@columbia.edu>
2026-03-28 15:22:09 -0400

    setup Next.js and MongoDB connection

---
+++ lib/mongodb.ts
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI as string;
+
+interface MongooseGlobal {
+  conn: typeof mongoose | null;
+  promise: Promise<typeof mongoose> | null;
+}
+
+const globalWithMongoose = global as typeof globalThis & {
+  mongoose: MongooseGlobal;
+};
+
+if (!globalWithMongoose.mongoose) {
+  globalWithMongoose.mongoose = { conn: null, promise: null };
+}
+
+const cached = globalWithMongoose.mongoose;
+
+export async function connectDB() {
+  if (cached.conn) return cached.conn;
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI).then((m) => m);
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
